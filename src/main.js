6cd457ea3840aabc43522a47f3c582fd54f2e476
Andrii <andrii.sliusarenko@gmail.com>
2024-09-05 02:06:55 +0300

    set template

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
+// import Swiper JS
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
