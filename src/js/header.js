24a31db886c11fdbc3ff5488f5a3994445d148c1
Nastya <nastyusha3@gmail.com>
2024-09-10 19:02:27 +0300

    header

--- src/js/header.js
+++ src/js/header.js
@@ -2,16 +2,13 @@ const mobileMenu = document.querySelector('.js-menu-container');
 const openMenuBtn = document.querySelector('.js-open-menu');
 const menuCloseBtn = document.querySelector('.js-close-menu');
 
-
 openMenuBtn.addEventListener('click', () => {
   mobileMenu.classList.add('is-open');
 });
 
-
 menuCloseBtn.addEventListener('click', () => {
   mobileMenu.classList.remove('is-open');
 });
-
 
 const navLinks = document.querySelectorAll('.mobile-nav-link');
 
@@ -20,14 +17,18 @@ function closeMenu() {
 }
 
 navLinks.forEach(link => {
-  link.addEventListener('click', (event) => {
+  link.addEventListener('click', event => {
     const targetSection = event.target.getAttribute('href');
-    document.querySelector(targetSection).scrollIntoView({ behavior: 'smooth' });
+    document
+      .querySelector(targetSection)
+      .scrollIntoView({ behavior: 'smooth' });
     closeMenu();
   });
 });
 
-document.querySelector('.header-menu-click').addEventListener('click', function () {
+document
+  .querySelector('.header-menu-click')
+  .addEventListener('click', function () {
     const menu = document.querySelector('.menu-click');
-    menu.classList.toggle('open'); 
-});
+    menu.classList.toggle('open');
+  });
